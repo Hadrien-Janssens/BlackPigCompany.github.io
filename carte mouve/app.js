cdf07439d49d4210065245fe5b9f594a86e23084
BlackPigCompany <86426768+BlackPigCompany@users.noreply.github.com>
2022-11-16 19:42:33 +0100

    update card move

--- carte mouve/app.js
+++ carte mouve/app.js
@@ -1,23 +1,79 @@
 const card = document.querySelector('.card');
 const container = document.querySelector(".container");
-console.log(container);
+
 container.addEventListener("mousemove",(e) => {
     let xAxis =(window.innerWidth/2 - e.pageX)/15;
     let yAxis =(window.innerHeight/2 - e.pageY)/15;
-    console.log(yAxis);
+    
     card.style.transform = `rotateY(${xAxis}deg) rotateX(${-yAxis}deg)`;
-    img.style.transition = '0.5s ease';
+    image.style.transition = '0.5s ease';
 });
-const img = document.querySelector('.img img');
+const image = document.querySelector('.img ');
 const titre= document.querySelector('h1');
 container.addEventListener('mouseenter',(e)=>{
     card.style.transition = '0s';
-    img.style.transform ='translateZ(70px) rotateZ(-30deg)' ;
+    image.style.transform ='translateZ(70px) rotateZ(-30deg)' ;
     titre.style.transform ='translateZ(20px)';
 });
 
 container.addEventListener('mouseleave',(e)=>{
     card.style.transform = `rotateY(0deg) rotateX(0deg)`;
     card.style.transition = '0.5s ease';
-    img.style.transform ='translateZ(0px) rotateZ(0deg)' ;
+    image.style.transform ='translateZ(0px) rotateZ(0deg)' ;
+});
+const vraiImage =document.querySelector('.img');
+const fanta = document.getElementById('fanta');
+fanta.addEventListener('click',(e)=>{
+    titre.innerText='Fanta';
+    document
+    .getElementById('fanta-img')
+    .classList.remove('hidden');
+    document
+    .getElementById('coca-img')
+    .classList.add('hidden');
+    document
+    .getElementById('sprite-img')
+    .classList.add('hidden');
+    fanta.classList.add('fantaactive');
+    coca.classList.remove('cocaactive');
+    sprite.classList.remove('spriteactive');
+
+});
+const sprite = document.getElementById('sprite');
+sprite.addEventListener('click',(e)=>{
+    titre.innerText='Sprite';
+    document
+    .getElementById('sprite-img')
+    .classList.remove('hidden');
+    document
+    .getElementById('coca-img')
+    .classList.add('hidden');
+    document
+    .getElementById('fanta-img')
+    .classList.add('hidden');
+    sprite.classList.add('spriteactive');
+    fanta.classList.remove('fantaactive');
+    coca.classList.remove('cocaactive')
+});
+const coca = document.getElementById('coca');
+coca.addEventListener('click',(e)=>{
+    titre.innerText='Coca-Cola';
+    document
+    .getElementById('fanta-img')
+    .classList.add('hidden');
+    document
+    .getElementById('sprite-img')
+    .classList.add('hidden');
+    document
+    .getElementById('coca-img')
+    .classList.remove('hidden');
+    coca.classList.add('cocaactive');
+    fanta.classList.remove('fantaactive');
+    sprite.classList.remove('spriteactive');
+});
+const water = document.getElementById('water');
+water.addEventListener('click',(e)=>{
+    titre.innerText='Water'
+    
+    
 });
