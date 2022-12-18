0af54553eadc592a274b05f99cf2c827392d9289
BlackPigCompany <86426768+BlackPigCompany@users.noreply.github.com>
2022-12-18 12:45:09 +0100

    button nav

--- frontend/inscription/inscription.js
+++ frontend/inscription/inscription.js
@@ -4,7 +4,7 @@ const inscriptionButton = document.getElementById('inscription');
 
 inscriptionButton.addEventListener('click',()=>{
     
-        fetch(`http://localhost:3000/api/auth/`,
+        fetch(`http://www.portfolio-hadrien.com/backend/auth/`,
     {
         headers: {
         'Accept': 'application/json',
@@ -16,7 +16,7 @@ inscriptionButton.addEventListener('click',()=>{
     })
     .then(function(res){
         console.log(res);
-        window.location.href ='http://127.0.0.1:5501/frontend/home.html';
+        window.location.href ='http://www.blackpigcompany.github.io/frontend/home.html';
         
     })
     .catch(function(res){ console.log(res) });
